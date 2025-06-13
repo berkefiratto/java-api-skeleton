e3fbd575a2268bd6afd7d3eda38db88dffa0ad00
Berke Fırat YILDIRIM <69518455+berkefiratto@users.noreply.github.com>
2025-06-13 22:25:08 +0300

    Create UserController.java
    Added UserController.java

---
+++ src/main/java/com/berke/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.berke.api.controller;
+
+import com.berke.api.dto.UserDto;
+import com.berke.api.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.createUser(userDto));
+    }
+}
